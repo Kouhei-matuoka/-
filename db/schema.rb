85b0f10d9402cfc93bc1770a067e317a358af910
kouhei <kouhei19951112@gmail.com>
2020-05-16 07:00:28 +0000

    Finish signup-user

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20200516034400) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "pasword_digest"
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
